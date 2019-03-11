471bc3b5b1aa5921855e3da90f1f64905ee39c79
Shadman Sakib <sakib.buet51@outlook.com>
2019-03-12 00:52:43 +0600

    Test Success

--- CryptoTest/Program.cs
+++ CryptoTest/Program.cs
@@ -9,19 +9,19 @@ namespace CryptoTest
         {
             var aes = new AESEncryption();
 
-            //var enc = aes.Encrypt("Simple text test", "0cb7add6af7f6798");
-            //Console.WriteLine(enc);
+            var enc = aes.Encrypt("Simple text test pad", "0cb7add6af7f6798");
+            Console.WriteLine(enc);
 
-            //var res = aes.Decrypt(enc, "0cb7add6af7f6798");
-            //Console.WriteLine(res);
+            var res = aes.Decrypt(enc, "0cb7add6af7f6798");
+            Console.WriteLine(res);
 
-            //res = aes.Decrypt("ZTa5NYNsSdBDGe/3r91I1Q==", "0cb7add6af7f6798");
+            res = aes.Decrypt("ZTa5NYNsSdBDGe/3r91I1Q==", "0cb7add6af7f6798");
 
-            //var enc = aes.Encrypt("Two One Nine Two", "Thats my Kung Fu");
-            //Console.WriteLine(enc);
+            enc = aes.Encrypt("Two One Nine Two", "Thats my Kung Fu");
+            Console.WriteLine(enc);
 
-            //var res = aes.Decrypt(enc, "Thats my Kung Fu");
-            //Console.WriteLine(res);
+            res = aes.Decrypt(enc, "Thats my Kung Fu");
+            Console.WriteLine(res);
 
 
             Console.ReadLine();
